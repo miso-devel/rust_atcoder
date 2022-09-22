be8a02ac9b5935c51adbcb1ff1b77a4c6861da7c
nisi0929 <kengo279@icloud.com>
2022-09-23 03:53:38 +0900

    atcoderrr

--- contests/abs/src/bin/abc081a.rs
+++ contests/abs/src/bin/abc081a.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s:String
+    }
+    let vec: Vec<char> = s.chars().collect();
+    let mut result: i64 = 0;
+    let match_pattern: char = '1';
+    for v in vec {
+        if v == match_pattern {
+            result += 1;
+        };
+    }
+    println!("{}", result)
 }
--- contests/abs/src/bin/practice-a.rs
+++ contests/abs/src/bin/practice-a.rs
@@ -2,5 +2,11 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        a:i64,
+        b:i64,
+        c:i64,
+        s:String
+    }
+    println!("{} {}", a + b + c, s)
 }
