cda5dcc97ada3d04aedfe048cb647630ed475af9
nisi0929 <kengo279@icloud.com>
2022-09-22 21:22:45 +0900

    vtdekinai..

---
+++ contests/abc238/src/bin/a.rs
@@ -0,0 +1,13 @@
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input! {
+        n:usize,
+    }
+    if n == 1 || n >= 5 {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
+}
