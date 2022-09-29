4e08ca84a28f1fd2dbb7732bd63bba6ab1bc6f5a
nisi0929 <kengo279@icloud.com>
2022-09-30 00:48:52 +0900

    vt

--- virtual_contests/doer0929/src/bin/2.rs
+++ virtual_contests/doer0929/src/bin/2.rs
@@ -2,5 +2,28 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n:usize,
+        a:usize,
+        b:usize,
+    mut p:[usize;n]
+    }
+    // 1 問目の配点は A 点以下、2 問目の配点は A+1 点以上 B 点以下、3 問目の配点は B+1 点以上である
+
+    let mut tmp1 = 0;
+    let mut tmp2 = 0;
+    let mut tmp3 = 0;
+    for v in p {
+        if v <= a {
+            tmp1 += 1
+        }
+        if v >= a + 1 && v <= b {
+            tmp2 += 1
+        }
+        if v >= b + 1 {
+            tmp3 += 1
+        }
+    }
+
+    println!("{}", [tmp1, tmp2, tmp3].iter().min().unwrap())
 }
--- virtual_contests/doer0929/src/bin/4.rs
+++ virtual_contests/doer0929/src/bin/4.rs
@@ -6,7 +6,9 @@ fn main() {
         n:i32,
         x:[i32;n]
     }
-
+    // bit全探索
+    // https://www.try-it.jp/chapters-4962/sections-4963/lessons-5016/
+    // https://atcoder.jp/contests/arc114/tasks/arc114_a
     for v in x {
         for i in 0..=50 {
             println!("{}", integer::gcd(v, i))
